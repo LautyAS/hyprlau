210a8d41c735887e19c1e92b736b90c4343aa230
lauti <lautaroanzstr@gmail.com>
2025-06-12 00:29:46 -0300

    Fixes4

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#mis fuentes favoritas son: ttf-firacode-nerd ttf-noto-nerd y ttf-sourcedodepro-nerd
 
 echo -e "\nA little reminder... If you have any trouble running this script, please, ensoure that you aren't running this as sudo, and that you own your /home directory, you can do that by typing (sudo chown -R "Your User" /home)\n" && sleep 1
 
@@ -7,8 +8,7 @@ read -p "Now that I said that.. Do you want to start? (Y/n): " strun
 
 if [[ "$strun" == "y" || "$strun" == "Y" ]]; then
 
-	pacman -Sy --needed git base-devel kitty
-
+	sudo pacman -Sy --needed --noconfirm git base-devel kitty
 	git clone https://aur.archlinux.org/paru-bin.git
 	cd paru-bin
 	echo "Estamos en la carpeta de paru" && sleep 5
@@ -17,25 +17,17 @@ if [[ "$strun" == "y" || "$strun" == "Y" ]]; then
 	rm -rf paru-bin
 
 	read -p "Do you want to apply a basic pacman config? (Y/N)" respuesta
-
 	if [[ "$respuesta" == "y" || "$respuesta" == "Y" ]]; then
 		echo "Nice!"
-		cat pacman.conf | sudo tee /etc/pacman.conf #> /dev/null
-		paru --noconfirm -Syu --needed base-devel kitty floorp-bin hyprland hyprpaper rofi dolphin waybar fastfetch github-cli alsa-lib alsa-utils pipewire pipewire-pulse pipewire-alsa pipewire-jack pipewire-media-session steam discord prismlauncher blender ly ttf-nerd-fonts-symbols ttf-firacode-nerd ttf-sourcecodepro-nerd ttf-noto-nerd noto-fonts spotify-launcher pavucontrol
-	
-	echo "firacode, noto and source code font were added, select what you like"
-	echo "Recoda q la config de waybar esta en /etc/xdg/waybar/config.jsonc" 
+		cat ~/hyprlau/pacman.conf | sudo tee /etc/pacman.conf
+		echo -e "\n\n\n\nVamos a instalar los paquetes...\n\n" && sleep 5
+		paru -Sy --noconfirm --needed base-devel kitty floorp-bin hyprland hyprpaper rofi dolphin waybar fastfetch github-cli alsa-lib alsa-utils pipewire pipewire-pulse pipewire-alsa pipewire-jack pipewire-media-session steam discord prismlauncher blender ly ttf-nerd-fonts-symbols ttf-noto-nerd ttf-sourcecodepro-nerd ttf-firacode-nerd noto-fonts spotify-launcher pavucontrol
 	elif [[ "$respuesta" == "n" || "$respuesta" == "N" ]]; then
 		echo "Ok, let's do the rest of this..."
-		paru --noconfirm -Syu --needed base-devel kitty floorp-bin hyprpaper rofi dolphin waybar fastfetch github-cli libreoffice alsa-lib alsa-utils pipewire pipewire-pulse pipewire-alsa pipewire-jack pipewire-media-session ly ttf-nerd-fonts-symbols ttf-firacode-nerd ttf-sourcecodepro-nerd ttf-noto-nerd1 noto-fonts spotify-launcher pavucontrol
-
+		paru -Sy --noconfirm --needed base-devel kitty floorp-bin hyprpaper rofi dolphin waybar fastfetch github-cli libreoffice alsa-lib alsa-utils pipewire pipewire-pulse pipewire-alsa pipewire-jack pipewire-media-session ly ttf-nerd-fonts-symbols ttf-noto-nerd ttf-firacode-nerd ttf-sourcedodepro-nerd noto-fonts spotify-launcher pavucontrol
 	fi
 	
 	cat ~/hyprlau/hyprland.conf | tee ~/.config/hypr/hyprland.conf
 	cat ~/hyprlau/waybarcfg.jsonc | sudo tee /etc/xdg/waybar/config.jsonc 
 	sudo systemctl enable ly
-
-#else
-#	echo "Then, see you later!"
-
 fi
