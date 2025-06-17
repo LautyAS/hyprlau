682fc802462561c0c822893ceed890dadd56ec13
LautyAS <lautaroanzstr@gmail.com>
2025-06-17 03:56:15 -0300

    Fixed config copying

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ if [[ "$strun" == "y" || "$strun" == "Y" ]]; then
 	sudo pacman -Sy --needed --noconfirm git base-devel kitty
 	git clone https://aur.archlinux.org/paru-bin.git
 	cd paru-bin
-	echo "Estamos en la carpeta de paru" && sleep 5
+	echo "Estamos en la carpeta de paru"
 	makepkg -si
 	cd ..
 	rm -rf paru-bin
@@ -20,17 +20,17 @@ if [[ "$strun" == "y" || "$strun" == "Y" ]]; then
 	if [[ "$respuesta" == "y" || "$respuesta" == "Y" ]]; then
 		echo "Nice!"
 		cat ~/hyprlau/configs/pacman.conf | sudo tee /etc/pacman.conf
-		paru -Sy --noconfirm --needed base-devel kitty floorp-bin hyprland hyprpaper fuzzel dolphin waybar fastfetch github-cli alsa-lib alsa-utils pipewire pipewire-pulse pipewire-alsa pipewire-jack wireplumber steam discord prismlauncher ly ttf-nerd-fonts-symbols ttf-noto-nerd ttf-sourcecodepro-nerd ttf-firacode-nerd noto-fonts spotify-launcher pavucontrol
+		paru -Syyu --noconfirm --needed base-devel kitty floorp-bin hyprland hyprpaper fuzzel dolphin waybar fastfetch github-cli alsa-lib alsa-utils pipewire pipewire-pulse pipewire-alsa pipewire-jack wireplumber steam discord prismlauncher ly ttf-nerd-fonts-symbols ttf-noto-nerd ttf-sourcecodepro-nerd ttf-firacode-nerd noto-fonts spotify-launcher pavucontrol
 	elif [[ "$respuesta" == "n" || "$respuesta" == "N" ]]; then
 		echo "Ok, let's do the rest of this..."
-		paru -Sy --noconfirm --needed base-devel kitty floorp-bin hyprpaper fuzzel dolphin waybar fastfetch github-cli libreoffice alsa-lib alsa-utils pipewire pipewire-pulse pipewire-alsa pipewire-jack wireplumber ly ttf-nerd-fonts-symbols ttf-noto-nerd ttf-firacode-nerd ttf-sourcedodepro-nerd noto-fonts spotify-launcher pavucontrol
+		paru -Syyu --noconfirm --needed base-devel kitty floorp-bin hyprpaper fuzzel dolphin waybar fastfetch github-cli libreoffice alsa-lib alsa-utils pipewire pipewire-pulse pipewire-alsa pipewire-jack wireplumber ly ttf-nerd-fonts-symbols ttf-noto-nerd ttf-firacode-nerd ttf-sourcedodepro-nerd noto-fonts spotify-launcher pavucontrol
 	fi
 	
 	systemctl --user mask at-spi-dbus-bus.service
-	cat ~/hyprlau/configs/fuzzel.ini | tee ~/.config/fuzzel.ini
-	cat ~/hyprlau/configs/hyprland.conf | tee ~/.config/hypr/hyprland.conf
-	cat ~/hyprlau/configs/waybarcfg.jsonc | sudo tee /etc/xdg/waybar/config.jsonc 
-	cat ~/hyprlau/configs/hyprpaper.conf | tee ~/.config/hypr/hyprpaper.conf
-	cp -r ~/hyprlau/rice /home/rice
+	cp ~/hyprlau/configs/fuzzel.ini ~/.config/fuzzel.ini
+	cp ~/hyprlau/configs/hyprland.conf ~/.config/hypr/hyprland.conf
+	sudo cp ~/hyprlau/configs/waybarcfg.jsonc /etc/xdg/waybar/config.jsonc 
+	cp ~/hyprlau/configs/hyprpaper.conf ~/.config/hypr/hyprpaper.conf
+	cp -r ~/hyprlau/rice ~/rice
 	sudo systemctl enable ly
 fi
